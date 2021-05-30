7cc27db9271f751444636f5d538a2a751d4c4d68
Daniel <dcmckay10@gmail.com>
2021-05-29 21:05:33 -0600

    Converted to stateful, not sure if I need

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:readme/pages/newsPage.dart';
-import 'package:readme/pages/readPage.dart';
+import 'package:readme/reader/Feed.dart';
+import 'package:readme/reader/reader.dart';
 import 'package:readme/theme/custom_theme.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Feed> feeds;
+
   @override
   Widget build(BuildContext context) {
+    getFeeds();
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: CustomTheme.darkTheme,
-      home: ReadPage(
-        "Hello Wolrd",
-        21,
-        "https://source.unsplash.com/random",
-        "Hello world"
-      ),
+      home: NewsPage(),
+      // home: ReadPage(
+      //   "Hello World",
+      //   21,
+      //   "https://source.unsplash.com/random",
+      //   "Hello world"
+      // ),
     );
+  }
+
+  void getFeeds() async {
+    Reader reader = Reader();
+    List<Feed> feeds = await reader.getFeed();
+    setState(() {
+      feeds = feeds;
+    });
   }
 }
